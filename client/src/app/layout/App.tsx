bdb8eb212e3faa60061a8d33da316fec9cec55cb
djquangsumi@gmail.com <djquangsumi@gmail.com>
2024-07-13 15:24:51 +0700

    end of section 7

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -6,11 +6,12 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { getCookie } from '../util/util';
 import agent from '../api/agent';
-import { useStoreContext } from '../context/StoreContext';
 import LoadingComponent from './LoadingComponent';
+import { useAppDispatch } from '../stores/configureStore';
+import { setBasket } from '../../features/basket/basketSlice';
 
 function App() {
-  const {setBasket} = useStoreContext();
+  const dispatch = useAppDispatch();
   const [loading, setLoading] = useState(true);
   const [darkMode, setDarkMode] = useState(false);
   const palleteType = darkMode ? 'dark' : 'light';
@@ -31,13 +32,13 @@ function App() {
     const buyerId = getCookie('buyerId');
     if (buyerId) {
       agent.Basket.get()
-        .then(basket => setBasket(basket))
+        .then(basket => dispatch(setBasket(basket)))
         .catch(error => console.log(error))
         .finally(() => setLoading(false));
     } else {
       setLoading(false);
     }
-  }, [setBasket])
+  }, [dispatch])
 
   if (loading) return <LoadingComponent message='Initiasing app...' />
 
